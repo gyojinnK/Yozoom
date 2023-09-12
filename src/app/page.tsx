63ce371fc29899a262f9e326a9ceb536a0102d67
gyojinnK <ksmug1998@gmail.com>
2023-09-13 01:52:02 +0900

    section 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import style from "./page.module.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Section from "../components/Section";
 
 export default function Home() {
-    return <h1>My Next</h1>;
+    return (
+        <>
+            <Section />
+        </>
+    );
 }
