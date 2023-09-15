5822c39123b2b62d78329d2c895ed25530491d12
gyojinnK <ksmug1998@gmail.com>
2023-09-16 01:21:25 +0900

    비사용 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import style from "./page.module.css";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import Section from "../components/Section";
 
 export default function Home() {
