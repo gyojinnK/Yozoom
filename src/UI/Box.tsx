2b60b633b2d7954d26742eb5291a9206f9d91867
gyojinnK <ksmug1998@gmail.com>
2023-09-16 01:20:07 +0900

    박스 UI 자체 미디어 쿼리 삽입

--- src/UI/Box.tsx
+++ src/UI/Box.tsx
@@ -4,11 +4,17 @@ import styled from "styled-components";
 
 const StyledBox = styled.div`
     width: 100%;
-    height: 190px;
+    height: ${(props) => props.height};
     background-color: var(--box-background-rgb);
     border-radius: 25px;
     box-shadow: rgba(0, 0, 0, 0.5) 0 5px 5px 0;
-    margin: 0;
+    margin: 0 0 20px 0;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    @media screen and (max-width: 800px) {
+        flex-direction: column;
+    }
 `;
 
 const Box = ({ children }: any) => {
