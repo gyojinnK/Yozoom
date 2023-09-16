ba0de73bcb408e4af1c2ec21e1206a0761051abf
gyojinnK <ksmug1998@gmail.com>
2023-09-16 16:35:33 +0900

    Search 코드 정리 및 수정

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,17 +1,25 @@
 import Box from "@/UI/Box";
-import "./Search.css";
 import Image from "next/image";
 import nLogo from "../../public/img/naver_logo.png";
-import styles from "./css_modules/text.module.css";
+import styles1 from "./css_modules/text.module.css";
+import styles2 from "./css_modules/layout.module.css";
 
 const Search = () => {
     return (
         <Box>
-            <div className="searchTxt">
-                <dt className={`${styles.title} ${styles.dt}`}>검색어 분석</dt>
-                <dd className={styles.dd}>대형 플랫폼의 검색을 분석합니다.</dd>
+            <div className={`${styles2.flexBox}`} style={{ height: "100px" }}>
+                <div className="searchTxt">
+                    <dt
+                        className={`${styles1.title} ${styles1.dt} ${styles2.dtLayout}`}
+                    >
+                        검색어 분석
+                    </dt>
+                    <dd className={`${styles1.dd} ${styles2.ddLayout}`}>
+                        대형 플랫폼의 검색을 분석합니다.
+                    </dd>
+                </div>
+                <Image src={nLogo} alt="logo" style={{ marginTop: "10px" }} />
             </div>
-            <Image src={nLogo} alt="logo" />
         </Box>
     );
 };
