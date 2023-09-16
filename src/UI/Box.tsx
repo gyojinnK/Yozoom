be4c9c445470fa3db4fa2f8deda41affd32b6b1a
gyojinnK <ksmug1998@gmail.com>
2023-09-16 16:34:10 +0900

    css 속성 값 수정

--- src/UI/Box.tsx
+++ src/UI/Box.tsx
@@ -1,20 +1,13 @@
 "use client";
-import React from "react";
 import styled from "styled-components";
 
 const StyledBox = styled.div`
     width: 100%;
-    height: ${(props) => props.height};
+    height: ${(props: any) => props.heights};
     background-color: var(--box-background-rgb);
     border-radius: 25px;
     box-shadow: rgba(0, 0, 0, 0.5) 0 5px 5px 0;
     margin: 0 0 20px 0;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    @media screen and (max-width: 800px) {
-        flex-direction: column;
-    }
 `;
 
 const Box = ({ children }: any) => {
