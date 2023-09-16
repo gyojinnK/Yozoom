3ad3fa700c5898de163ff7cf050b7613d72cb89e
gyojinnK <ksmug1998@gmail.com>
2023-09-16 16:35:57 +0900

    SnsTrend 컴포넌트 추가

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,12 +1,14 @@
 "use client";
 import Intro from "./Intro";
 import Search from "./Search";
+import SnsTrend from "./SnsTrend";
 
 const Section = () => {
     return (
         <>
             <Intro />
             <Search />
+            <SnsTrend />
         </>
     );
 };
