58851af2ef198f21e90d8a5d162897469455d896
gyojinnK <ksmug1998@gmail.com>
2023-09-25 17:21:00 +0900

    오류 수정중

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -15,25 +15,23 @@ const Intro = () => {
                     content="initial-scale=1, width=device-width"
                 />
             </Head>
-            <Box>
-                <div className={`${layoutStyles.flexBox} ${css.introWrap}`}>
-                    <div className={css.descriptionTxt}>
-                        <dt className={textStyle.dt}>요즘 트렌드를 Zoom!</dt>
-                        <dd className={textStyle.dd}>
-                            분석된 다양한 트렌드를 만나보세요. <br />
-                            선두주자가 되는 길은 여기에 있습니다.
-                        </dd>
-                    </div>
-                    <Image
-                        className={css.img}
-                        src={logo}
-                        alt="logo of service"
-                        width={300}
-                        height={58}
-                        priority={false}
-                    />
+            <div className={`${layoutStyles.flexBox} ${css.introWrap}`}>
+                <div className={css.descriptionTxt}>
+                    <dt className={textStyle.dt}>요즘 트렌드를 Zoom!</dt>
+                    <dd className={textStyle.dd}>
+                        분석된 다양한 트렌드를 만나보세요. <br />
+                        선두주자가 되는 길은 여기에 있습니다.
+                    </dd>
                 </div>
-            </Box>
+                <Image
+                    className={css.img}
+                    src={logo}
+                    alt="logo of service"
+                    width={300}
+                    height={58}
+                    priority={false}
+                />
+            </div>
         </>
     );
 };
