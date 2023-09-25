48f9c9c018737975a744e9a54eb048457fa66d4b
gyojinnK <ksmug1998@gmail.com>
2023-09-25 17:25:23 +0900

    오류 수정중

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import logo from "../../public/img/logo.png";
-import "./Header.css";
+import css from "@/styles/Header.module.css";
 
 export default function Header() {
     return (
-        <header>
+        <header className={css.header}>
             <Image src={logo} alt="logo of yozoom" width={140} />
-            <nav>
-                <ul>
+            <nav className={css.headerNav}>
+                <ul className={css.headerNavUl}>
                     <li>Searchs</li>
                     <li>Trends</li>
                     <li>MyTrend</li>
