abb4557697cd5e3c3fdf04cbf1b6125522e48c70
gyojinnK <ksmug1998@gmail.com>
2023-09-28 17:54:04 +0900

    BE API 요청 Hook사용

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,9 +1,15 @@
 "use client";
+import { useEffect } from "react";
 import Intro from "./Intro";
 import Search from "./Search";
 import SnsTrend from "./SnsTrend";
+import { getDataToDj } from "@/hooks/GetDataTest";
 
 const Section = () => {
+    useEffect(() => {
+        getDataToDj();
+    }, []);
+
     return (
         <>
             <Intro />
