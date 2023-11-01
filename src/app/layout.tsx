6b3d19452bcc22bdbb9539667562a750d3a725fe
gyojinnK <ksmug1998@gmail.com>
2023-11-01 18:41:07 +0900

    레이아웃 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Head from "next/head";
+import Intro from "@/components/Intro";
 
 export const metadata: Metadata = {
     title: "요즘 트렌트를 Zoom - Yozoom",
@@ -25,6 +26,7 @@ export default function RootLayout({
             <body>
                 <div className="homeWrap">
                     <Header />
+                    <Intro />
                     {children}
                     <Footer />
                 </div>
