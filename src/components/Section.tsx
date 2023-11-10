723d0f8687034fd3e57a6c46f8279e22d0f67f70
gyojinnK <ksmug1998@gmail.com>
2023-11-11 01:42:06 +0900

    테스트 코드 삭제

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,15 +1,9 @@
 "use client";
-import { useEffect } from "react";
-import { getDataToDj } from "@/hooks/GetDataTest";
 import RelativeTopic from "./RelativeTopic";
 import Timelapse from "./Timelapse";
 import AiProccessing from "./AiProccessing";
 
 const Section = () => {
-    useEffect(() => {
-        getDataToDj();
-    }, []);
-
     return (
         <>
             <RelativeTopic />
