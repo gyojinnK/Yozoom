8243995b6ba335ad5a831b5e181079d995186192
gyojinnK <ksmug1998@gmail.com>
2023-11-16 00:17:55 +0900

    스타일 추가 및 코드 수정

--- src/components/RelativeTopicView.tsx
+++ src/components/RelativeTopicView.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import tvCss from "@/styles/RelativeTopicView.module.css";
 
 const RelativeTopicView = (props: any) => {
     const [topDatas, setTopDatas] = useState([{ title: "", type: "" }]);
@@ -34,34 +35,63 @@ const RelativeTopicView = (props: any) => {
     }, [props.data]);
 
     return (
-        <div
-            style={{
-                display: "flex",
-                margin: "0 auto",
-                width: "90%",
-                justifyContent: "space-evenly",
-            }}
-        >
-            <div>
-                [top]
-                {topDatas.map((item: any) => {
-                    return (
-                        <li key={item.title}>
-                            {item.type} {">"} {item.title}
-                        </li>
-                    );
-                })}
+        <div className={tvCss.wrap}>
+            <div className={tvCss.subWrap}>
+                <p>상위</p>
+                <ul>
+                    {topDatas.map((item: any) => {
+                        return (
+                            <li key={item.title}>
+                                <span className={tvCss.type}>{item.type}</span>
+                                <span className={tvCss.title}>
+                                    {item.title}
+                                </span>
+                            </li>
+                        );
+                    })}
+                </ul>
             </div>
-            <div>
-                [rising]
-                {risingDatas.map((item: any) => {
-                    return (
-                        <li key={item.title}>
-                            {item.type} {">"} {item.title}
-                        </li>
-                    );
-                })}
+            <div className={tvCss.subWrap}>
+                <p>급등</p>
+                <ul>
+                    {risingDatas.map((item: any) => {
+                        return (
+                            <li key={item.title}>
+                                <span className={tvCss.type}>{item.type}</span>
+                                <span className={tvCss.title}>
+                                    {item.title}
+                                </span>
+                            </li>
+                        );
+                    })}
+                </ul>
             </div>
+            <style jsx>{`
+                p {
+                    margin: 0;
+                    color: var(--background-rgb);
+                    font-size: 1.5rem;
+                    font-weight: 700;
+                    padding: 10px 0 0 10px;
+                }
+                ul,
+                li {
+                    margin: 0;
+                    padding: 0;
+                }
+                ul {
+                    list-style: none;
+                    text-align: center;
+                    margin: 0 auto;
+                }
+                li {
+                    width: 280px;
+                    border-radius: 20px;
+                    box-shadow: rgba(0, 0, 0, 0.5) 0 5px 5px 0;
+                    margin: 10px auto;
+                    background-color: var(--background-rgb);
+                }
+            `}</style>
         </div>
     );
 };
